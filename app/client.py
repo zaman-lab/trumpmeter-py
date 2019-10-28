fda3e7a6a91556c69e1eb6e5a0a220632a687b1e
Mike Rossetti <s2t2@users.noreply.github.com>
2019-10-28 13:15:27 -0400

    Reconstruct final model from saved model weights

--- app/client.py
+++ app/client.py
@@ -3,6 +3,7 @@ from pprint import pprint
 import os
 
 from app.model import original_model, final_model
+from app.reconstruction import reconstructed_model
 from app.helper_text import main_clean
 
 def classify(txt, model):
@@ -17,37 +18,42 @@ def classify(txt, model):
     #return {"text": txt, "pro_trump": results[0], "other_num": results[1]} # not really sure if these are correct assignments
     return {"text": txt, "pro_trump": results[0], "temp": [round(float(score), 4) for score in results] }
 
-
 if __name__ == "__main__":
 
     model_o = original_model()
     model_f = final_model()
+    model_r = reconstructed_model()
 
     example_tweets = [
         "Trump for President!!! #MAGA",
         "Trump is the best ever!",
         "RT @someuser: Trump is, by far, the best POTUS in history. \n\nBonus: He^s friggin^ awesome!\n\nTrump gave Pelosi and the Dems the ultimate\u2026 ",
         "If Clinton is elected, I'm moving to Canada",
+        "Trump is doing a great job so far. Keep it up man.",
 
         "He is awesome, make american great again. Democrats is taking off. We love democrats.",
+        "This tweet is about basketball, and I'm watching tonight on CBS Sports",
 
         "Hillary for President!!! #StrongerTogether",
         "Trump is the worst ever!",
         "If Trump is elected, I'm moving to Canada",
-        "RT @MotherJones: A scientist who resisted Trump administration censorship of climate report just lost her job"
+        "RT @MotherJones: A scientist who resisted Trump administration censorship of climate report just lost her job",
+        "Trump is doing a terrible job so far. Vote him out ASAP."
     ]
 
     for twt in example_tweets:
-        print("-----------------------------")
+        print("\n-----------------------------")
         print(f"TWEET: '{twt}'")
         response_o = classify(twt, model_o)
         print("ORIG:", response_o["temp"])
         response_f = classify(twt, model_f)
         print("FINAL:", response_f["temp"])
+        response_r = classify(twt, model_r)
+        print("RECONST:", response_r["temp"])
 
     exit()
 
-    model = model_o
+    model = model_r
     while True:
         user_text = input("Your Text (press ENTER at any time to quit): ")
         if user_text in ["", "exit", "exit()"]: break
---
+++ app/reconstruction.py
@@ -0,0 +1,23 @@
+
+import os
+
+from app.model import final_model, unweighted_model
+
+RECONSTRUCTED_WEIGHTS_FILEPATH = os.path.join(os.path.dirname(__file__), "..", "model", "weights", "weights-reconstructed.hdf5")
+
+def recreate_weights(weights_filepath):
+    fm = final_model()
+    fm.save_weights(weights_filepath)
+
+def reconstructed_model(idempotent=True):
+    if idempotent == False:
+        recreate_weights(RECONSTRUCTED_WEIGHTS_FILEPATH)
+    rm = unweighted_model()
+    rm.load_weights(RECONSTRUCTED_WEIGHTS_FILEPATH)
+    return rm
+
+if __name__ == "__main__":
+
+    fm = final_model()
+    # had some issues uploading the final model to GCS, but maybe the weights file is uploadable...
+    rm = reconstructed_model(idempotent=False)
